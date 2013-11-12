e0b0423228f70047810c14f6a2ca6dff63201414
Jax <jax.gibb@gmail.com>
2013-11-13 07:02:52 +1100

    Initial

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+C:\Users\Jax\Documents\Development\Cordova\dummy\platforms\android\gen\com\jondarr\test\dummy\R.java \
+ : C:\Users\Jax\Documents\Development\Cordova\dummy\platforms\android\res\drawable\icon.png \
+C:\Users\Jax\Documents\Development\Cordova\dummy\platforms\android\res\drawable-hdpi\icon.png \
+C:\Users\Jax\Documents\Development\Cordova\dummy\platforms\android\res\drawable-ldpi\icon.png \
+C:\Users\Jax\Documents\Development\Cordova\dummy\platforms\android\res\drawable-mdpi\icon.png \
+C:\Users\Jax\Documents\Development\Cordova\dummy\platforms\android\res\drawable-xhdpi\icon.png \
+C:\Users\Jax\Documents\Development\Cordova\dummy\platforms\android\res\values\strings.xml \
+C:\Users\Jax\Documents\Development\Cordova\dummy\platforms\android\res\xml\config.xml \
+C:\Users\Jax\Documents\Development\Cordova\dummy\platforms\android\bin\AndroidManifest.xml \
---
+++ platforms/android/gen/com/jondarr/test/dummy/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.jondarr.test.dummy;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
